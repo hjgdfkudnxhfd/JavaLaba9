e5eca4dbbb481f19bfa6c238eb0c1cec421c5424
Diana <lici4qweru090@gmail.com>
2024-05-12 16:47:57 +0500

    commit 1

---
+++ src/main/java/com/example/javalaba9/JavaLaba9Application.java
@@ -0,0 +1,13 @@
+package com.example.javalaba9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLaba9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLaba9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javalaba9/JavaLaba9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javalaba9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLaba9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
